dc61dd279f53b956d59ea4da3c65f350df4103c7
Alexander Osorio <alextrespalaciososorio@gmail.com>
2025-03-29 17:29:18 -0500

    Obtaining samuel changes in the footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { BusinessComponent } from './pages/business/business.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { LogInComponent } from './pages/log-in/log-in.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'business', component: BusinessComponent }
+    { path: 'business', component: BusinessComponent },
+    { path: 'sign-in', component: SignInComponent },
+    { path: 'log-in', component: LogInComponent },
+    { path: 'about-us', component: AboutUsComponent },
 ];
