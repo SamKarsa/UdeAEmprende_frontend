e61a9799b8beafb75488254528a2fd4dbd99e54c
Alexander Osorio <alextrespalaciososorio@gmail.com>
2025-03-29 09:57:20 -0500

    Starting header component

--- src/app/pages/business/business.component.ts
+++ src/app/pages/business/business.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-business',
   imports: [HeaderComponent],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  templateUrl: './business.component.html',
+  styleUrl: './business.component.css'
 })
-export class HomeComponent {
+export class BusinessComponent {
 
 }
