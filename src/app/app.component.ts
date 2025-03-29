53ba025f810b507eb5fa57f7c648f2f7ca179363
SamKarsa <samuellopezmarin2006@gmail.com>
2025-03-29 18:17:17 -0500

    Add link to the footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
