e0603ca65a7ce11f02beec806d0855fd6bd5367a
SamKarsa <samuellopezmarin2006@gmail.com>
2025-03-29 17:10:33 -0500

    Finish Footer Again, but this time is true

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
-import { FooterssComponent } from './components/footerss/footerss.component'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, FooterssComponent],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
