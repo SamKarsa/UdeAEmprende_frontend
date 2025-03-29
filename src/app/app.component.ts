7ef71f6c31ed3fb736916b9e1ceba95fc565a480
SamKarsa <samuellopezmarin2006@gmail.com>
2025-03-29 12:27:24 -0500

    I advance part of the footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
+import { FooterssComponent } from './components/footerss/footerss.component'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, FooterComponent, FooterssComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/footerss/footerss.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footerss',
+  imports: [],
+  templateUrl: './footerss.component.html',
+  styleUrl: './footerss.component.css'
+})
+export class FooterssComponent {
+
+}
