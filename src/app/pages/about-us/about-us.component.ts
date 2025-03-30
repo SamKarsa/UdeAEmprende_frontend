6bcab5007f3b2298622a9f04c278b0286115d127
Alexander Osorio <alextrespalaciososorio@gmail.com>
2025-03-29 19:34:49 -0500

    Header finished

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-about-us',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
