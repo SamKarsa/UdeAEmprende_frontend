56e289dfa84541a59417733309b5e9fa333e7c45
SamKarsa <samuellopezmarin2006@gmail.com>
2025-03-29 20:03:03 -0500

    Reorganize to slider home

--- src/app/pages/business/business.component.ts
+++ src/app/pages/business/business.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-business',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './business.component.html',
   styleUrl: './business.component.css'
 })
