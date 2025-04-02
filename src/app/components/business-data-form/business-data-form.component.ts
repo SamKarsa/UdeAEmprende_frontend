6f3ebf7a478f9f747a250f0925ae01d1653db70f
SamKarsa <samuellopezmarin2006@gmail.com>
2025-04-02 12:55:23 -0500

    Finish basic structure to personal data and business data css

--- src/app/components/business-data-form/business-data-form.component.ts
+++ src/app/components/business-data-form/business-data-form.component.ts
@@ -7,21 +7,4 @@ import { Component } from '@angular/core';
   styleUrl: './business-data-form.component.css'
 })
 export class BusinessDataFormComponent {
-  autoResize(event: any) {
-    const textarea = event.target;
-    
-    // Guardamos posición del cursor/selección
-    const { selectionStart, selectionEnd, scrollTop } = textarea;
-    
-    textarea.style.height = 'auto';
-    textarea.style.height = textarea.scrollHeight + 'px';
-    
-    // Restauramos posición
-    textarea.scrollTop = scrollTop;
-    
-    // Si había texto seleccionado, lo restauramos
-    if (selectionStart !== selectionEnd) {
-      textarea.setSelectionRange(selectionStart, selectionEnd);
-    }
-  }
 }
